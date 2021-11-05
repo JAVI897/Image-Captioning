9ea61be7cbbb8308ec159c04b36a0eba8d804f8d
Javi GG <s.javi.com@gmail.com>
2021-11-05 16:34:48 +0100

    Update model.py

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ from settings import *
 def get_cnn_model():
     base_model = efficientnet.EfficientNetB0(
         input_shape=(*IMAGE_SIZE, 3), include_top=False, weights="imagenet",
-    ) # pesos image_net
+    ) # pesos image_net sin incluir top
     # Freeze feature extractor layers
     base_model.trainable = False
     base_model_out = base_model.output
