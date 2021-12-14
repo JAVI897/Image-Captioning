5256547fbfd06ee518c3a80cd99c2f86cc8e00c9
Javi GG <s.javi.com@gmail.com>
2021-12-14 11:22:36 +0100

    Update karpathy.py

--- karpathy.py
+++ karpathy.py
@@ -11,6 +11,7 @@ from nltk.translate.bleu_score import sentence_bleu as bleu_score
 from nltk.translate.meteor_score import single_meteor_score
 from nltk import word_tokenize
 import nltk
+from rouge_score import rouge_scorer
 nltk.download('punkt')
 nltk.download('wordnet')
 
@@ -71,6 +72,8 @@ BLEU_3 = 0
 BLEU_4 = 0
 BLEU_comb = 0
 METEOR = 0
+ROUGE_L = 0
+
 for index, row in df_results.iterrows():
     caption1, caption2, caption3, caption4, caption5, prediction = row['caption 1'], row['caption 2'], row['caption 3'], row['caption 4'], row['caption 5'], row['prediction']
     #references = [caption1.replace('.', '').split(), caption2.replace('.', '').split(), 
@@ -82,7 +85,6 @@ for index, row in df_results.iterrows():
     candidate = word_tokenize(prediction)
 
     # BLEU
-
     bleu_1 = bleu_score(references, candidate, weights=(1, 0, 0, 0))
     bleu_2 = bleu_score(references, candidate, weights=(0, 1, 0, 0))
     bleu_3 = bleu_score(references, candidate, weights=(0, 0, 1, 0))
@@ -103,6 +105,15 @@ for index, row in df_results.iterrows():
     meteor = meteor/5
     METEOR += meteor
 
+    # ROUGE-L
+    rouge_l = 0
+    for h, r in zip([candidate]*5, references):
+        scorer = rouge_scorer.RougeScorer(['rougeL'], use_stemmer=True)
+        score = scorer.score(r, h)['rougeL']
+        score = score.fmeasure
+        rouge_l += score
+    rouge_l = rouge_l/5
+    ROUGE_L += rouge_l
 
 BLEU_1 = BLEU_1/N
 BLEU_2 = BLEU_2/N
@@ -110,6 +121,7 @@ BLEU_3 = BLEU_3/N
 BLEU_4 = BLEU_4/N
 BLEU_comb = BLEU_comb/N
 METEOR = METEOR/N
+ROUGE_L = ROUGE_L/N
 
 df_scores = pd.DataFrame({'bleu_1': [BLEU_1], 'bleu_2': [BLEU_2], 
                           'bleu_3': [BLEU_3], 'bleu_4': [BLEU_4],
@@ -117,4 +129,4 @@ df_scores = pd.DataFrame({'bleu_1': [BLEU_1], 'bleu_2': [BLEU_2],
 
 df_scores.to_csv('scores_karpathy_test_predictions_{}_{}.csv'.format(CNN_TOP_MODEL, EMBED_DIM))
 
-print('[INFO] Scores. Bleu 1 = {:.4} Bleu 2 = {:.4} Bleu 3 = {:.4} Bleu 4 = {:.4} Bleu_comb = {:.4} METEOR = {:.4}'.format(BLEU_1, BLEU_2, BLEU_3, BLEU_4, BLEU_comb, METEOR))
+print('[INFO] Scores. Bleu 1 = {:.4} Bleu 2 = {:.4} Bleu 3 = {:.4} Bleu 4 = {:.4} Bleu_comb = {:.4} METEOR = {:.4} ROUGE_L = {:.4}'.format(BLEU_1, BLEU_2, BLEU_3, BLEU_4, BLEU_comb, METEOR, ROUGE_L))
